b3fea060ef248449254d2b036af87fe96ab8f622
Aashni Shah <contact@aashni.me>
2017-03-01 02:09:06 -0800

    displaying movie list, added bootstrap

--- controllers/mainCtrl.js
+++ controllers/mainCtrl.js
@@ -2,9 +2,6 @@ angularApp.controller('MainCtrl', [
 	'$firebaseObject',
 	'$scope',
 	function($firebaseObject, $scope){
-		$scope.heading = "Hello World";
-		$scope.message = "This is me";
-
 		/* firebase */
 		var firebase = new Firebase("https://movies-84898.firebaseio.com/movies");
 		$scope.movies = $firebaseObject(firebase);
